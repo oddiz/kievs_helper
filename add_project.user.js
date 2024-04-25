e98aff96eac4a8410958217c9d101222b07edd26
oddiz <kaansarkaya@gmail.com>
2024-04-25 17:18:32 +0300

    project name parser

--- add_project.user.js
+++ add_project.user.js
@@ -9,6 +9,7 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_xmlhttpRequest
+// @grant        GM_setClipboard
 // @require      https://github.com/AugmentedWeb/UserGui/raw/Release-1.0/usergui.js
 // @require      file://C:/Users/kaans/Desktop/Dev/kevser/tampermonkey/add_project.user.js
 // @run-at       document-end
@@ -18,7 +19,7 @@
   "use strict";
 
   const Gui = new UserGui();
-  Gui.settings.window.title = "GUI Demo"; // set window title
+  Gui.settings.window.title = "Kievs Helper"; // set window title
   Gui.settings.window.centered = true;
 
   Gui.addPage(
@@ -33,17 +34,17 @@
     </div>
     <div class="formbuilder-textarea form-group field-textarea-project-name">
         <label for="textarea-project-name" class="formbuilder-textarea-label">Project Name</label>
-        <textarea type="textarea" class="form-control" name="textarea-project-name" access="false" rows="1" id="textarea-project-name"></textarea>
+        <textarea type="textarea" class="form-control" name="textarea-project-name" access="false" rows="3" id="textarea-project-name"></textarea>
     </div>
-    <div class="formbuilder-button form-group field-button-projectname">
-        <button type="button" class="btn-info btn" name="button-projectname" access="false" style="info" id="button-projectname">Copy Project Name</button>
+    <div class="formbuilder-button form-group field-button-copy-projectname">
+        <button type="button" class="btn-info btn" name="button-copy-projectname" access="false" style="info" id="button-copy-projectname">Copy Project Name</button>
     </div>
     <div class="formbuilder-text form-group field-text-workorder">
         <label for="text-workorder" class="formbuilder-text-label">Work Order</label>
         <input type="text" class="form-control disabled" name="text-workorder" access="false" id="text-workorder">
     </div>
-    <div class="formbuilder-button form-group field-button-workorder">
-        <button type="button" class="btn-info btn" name="button-workorder" access="false" style="info" id="button-workorder">Copy WO</button>
+    <div class="formbuilder-button form-group field-button-copy-workorder">
+        <button type="button" class="btn-info btn" name="button-copy-workorder" access="false" style="info" id="button-copy-workorder">Copy WO</button>
     </div>
     <div class="formbuilder-text form-group field-text-er">
         <label for="text-er" class="formbuilder-text-label">ER</label>
@@ -62,7 +63,11 @@
 </div>`
   );
   const openBtn = document.createElement("button");
-  openBtn.innerText = "Open GUI";
+  openBtn.innerText = "Kievs Helper";
+  openBtn.style.display = "block";
+  openBtn.style.position = "fixed";
+  openBtn.style.top = "10px";
+  openBtn.style.right = "10px";
   openBtn.onclick = () => {
     openGui();
   };
@@ -80,6 +85,25 @@
         Gui.setValue("text-sp", result.spNumber);
       });
 
+      Gui.event("button-copy-projectname", "click", () => {
+        const projectName = Gui.getValue("textarea-project-name");
+        GM_setClipboard(projectName);
+      });
+      Gui.event("button-copy-workorder", "click", () => {
+        const workOrderNumber = Gui.getValue("text-workorder");
+        GM_setClipboard(workOrderNumber);
+      });
+      Gui.event("button-copy-er", "click", () => {
+        const erNumber = Gui.getValue("text-er");
+        console.log(erNumber);
+
+        GM_setClipboard(erNumber);
+      });
+      Gui.event("button-copy-sp", "click", () => {
+        const spNumber = Gui.getValue("text-sp");
+        GM_setClipboard(spNumber);
+      });
+
       Gui.loadConfig();
     });
   }
@@ -88,19 +112,19 @@
 function parseData(inputString) {
   // Extract the project name
   const projectNameMatch = inputString.match(/^(.*?)\:/);
-  const projectName = projectNameMatch ? projectNameMatch[1] : null;
+  const projectName = projectNameMatch ? projectNameMatch[1] : "olmadi 😟";
 
   // Extract the work order number
   const workOrderMatch = inputString.match(/WO\s(\d+)/);
-  const workOrderNumber = workOrderMatch ? workOrderMatch[1] : null;
+  const workOrderNumber = workOrderMatch ? workOrderMatch[1] : "";
 
   // Extract the ER number
   const erMatch = inputString.match(/ER\s(\d+)/);
-  const erNumber = erMatch ? erMatch[1] : null;
+  const erNumber = erMatch ? erMatch[1] : "";
 
   // Extract the SP number
   const spMatch = inputString.match(/SP\s([A-Z0-9]+)/);
-  const spNumber = spMatch ? spMatch[1] : null;
+  const spNumber = spMatch ? spMatch[1] : "";
 
   // Print the values
   console.log(`Project Name: ${projectName}`);
@@ -115,9 +139,9 @@ function parseData(inputString) {
   // Return the formatted string in case it needs to be used elsewhere
   return {
     projectName,
-    workOrderNumber,
-    erNumber,
-    spNumber,
+    workOrderNumber: workOrderNumber,
+    erNumber: erNumber,
+    spNumber: spNumber,
     formattedString,
   };
 }
