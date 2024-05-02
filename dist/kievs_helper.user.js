10f54891de5e9e0e58aed115871bb426ba734730
oddiz <kaansarkaya@gmail.com>
2024-05-03 02:33:40 +0300

    duzeltmece

--- dist/kievs_helper.user.js
+++ dist/kievs_helper.user.js
@@ -1,16 +1,20 @@
 // ==UserScript==
-// @name         Add Project Helper
-// @namespace    www.hwpowere.com/
-// @version      0.1
-// @description  Proce ekleme kolayligisi
-// @author       Koan
-// @match        *://hpowereuat.azurewebsites.net/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        GM_getValue
-// @grant        GM_setValue
-// @grant        GM_xmlhttpRequest
-// @grant        GM_setClipboard
-// @require      https://github.com/AugmentedWeb/UserGui/raw/Release-1.0/usergui.js
-// @require      https://github.com/oddiz/kievs_helper/raw/main/add_project.user.js
-// @run-at       document-end
+// @name          Add Project Helper
+// @namespace     www.hwpowere.com/
+// @version       0.1
+// @description   Proce ekleme kolayligisi
+// @author        Koan
+// @match         *://hpowereuat.azurewebsites.net/*
+// @match         *://hpoweredev.azurewebsites.net/*
+// @icon          data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @resource      BT_ICONS https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css
+// @grant         GM_getValue
+// @grant         GM_setValue
+// @grant         GM_xmlhttpRequest
+// @grant         GM_setClipboard
+// @grant         GM_getResourceText
+// @grant         GM_addStyle
+// @require       https://github.com/AugmentedWeb/UserGui/raw/Release-1.0/usergui.js
+// @require       https://github.com/oddiz/kievs_helper/raw/main/add_project.user.js
+// @run-at        document-end
 // ==/UserScript==
